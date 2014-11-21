66a8590e26521aeb3ea746d45d594f6d55d1e1ba
Sanjay <skpatel20@gmail.com>
2014-11-21 17:44:08 +0530

    Added some comment

--- src/main/java/com/naturalprogrammer/mymongoa/Application.java
+++ src/main/java/com/naturalprogrammer/mymongoa/Application.java
@@ -15,6 +15,7 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 public class Application {
 
     public static void main(String[] args) {
+    	// Run Spring
         SpringApplication.run(Application.class, args);
     }
 }
